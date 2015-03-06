05909174bdefe72d46aa1de6271664aaaebe3404
Luciano Daniel Diamand <lucianodiamand@yahoo.com>
2015-03-06 06:39:04 -0300

    Conflicto de nombre con las prácticas de clase.

---
+++ Clase02_ldiamand/src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		int numero; // Declaro la variable
+		numero = 1; // Inicializo la variable
+		
+		int numero2 = 2;
+		
+		char caracter = 'a';
+		String palabra;
+		palabra = "Hola Mundo";
+		System.out.println(palabra);
+		String enMinusculas = palabra.toLowerCase();
+		System.out.println(enMinusculas);
+	}
+}
