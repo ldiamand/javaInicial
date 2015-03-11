b21b489e13acfb606f6c51d0c4eea4a68ab7c990
Luciano Daniel Diamand <lucianodiamand@yahoo.com>
2015-03-11 13:08:07 -0300

    Ejemplo de do-while·

---
+++ Clase05_ldiamand/src/DoWhileDemo.java
@@ -0,0 +1,16 @@
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// do {
+		//     // cuerpo del do
+		// } while (cond. bool);
+		
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i < 10);
+		
+	}
+}
